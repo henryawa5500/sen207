7b82687898cda8e470b1ae1a100bbf6551b8c13f
henryawa5500 <116797090+henryawa5500@users.noreply.github.com>
2022-10-28 07:30:15 +0100

    Add files via upload

---
+++ sen207 assignment/index.js
@@ -0,0 +1,43 @@
+const cars = "toyota"
+console.log(cars)
+
+let books = "Maths"
+console.log(books)
+var name = "Henry"
+console.log(name)
+
+let boy ="henry"
+console.log("hello" + boy)
+
+//check the value of a variable
+console.log(typeof(boy))
+
+let number = 12
+
+const arr = ["henry", "victor", "peter"]
+
+console.log(typeof(number))
+console.log(typeof(arr))
+
+const henry = {
+    fristname : "awa",
+    lastname : "peter",
+    null : "henry@gmail.com"
+
+}
+console.log(henry.fristname)
+const david = {
+    name : "wawa",
+    age : "17",
+    country : "Nigeria",
+
+}
+console.log(david.name)
+const car = {
+    name : "toyota",
+    model : "2020 model",
+    weight : "500kg",
+    color : "black"
+
+}
+console.log(car.model)
